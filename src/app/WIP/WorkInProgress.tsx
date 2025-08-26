05152a07bf3d2732ccc6295a9334afa5c539f1d8
Gerudah <62789547+Gerudah@users.noreply.github.com>
2025-08-26 22:15:46 +1000

    Setting up Work in progress page

---
+++ src/app/WIP/WorkInProgress.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Image from '/Home/gerudah/repos/cwa-assignment/public/construction.jpg'
+
+const WorkInProgress = () => {
+  return (
+    <div>WorkInProgress</div>
+  )
+}
+
+export default WorkInProgress
